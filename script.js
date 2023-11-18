f80c8588fbe3fcada82515727119172a65078246
Garikapatitharani <150914712+Garikapatitharani@users.noreply.github.com>
2023-11-18 21:33:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function getWeather() {
+    const locationInput = document.getElementById('location');
+    const location = locationInput.value;
+
+    if (location.trim() === '') {
+        alert('Please enter a location.');
+        return;
+    }
+
+    const apiKey = 'YOUR_API_KEY'; // Replace with your OpenWeatherMap API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+}
+
+function displayWeather(data) {
+    const weatherInfoDiv = document.getElementById('weather-info');
+    weatherInfoDiv.innerHTML = `
+        <h2>${data.name}, ${data.sys.country}</h2>
+        <p>Temperature: ${data.main.temp}°C</p>
+        <p>Weather: ${data.weather[0].description}</p>
+        <p>Humidity: ${data.main.humidity}%</p>
+        <p>Wind Speed: ${data.wind.speed} m/s</p>
+    `;
+}
